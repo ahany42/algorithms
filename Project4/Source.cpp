ca4072062661ae36755a56ab9332554e66db623f
Ahany42 <ahanyfathy42@gmail.com>
2024-05-06 19:38:59 +0300

    Find Peak Algortithm

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 template <class T>
 void insertionSort(T arr[], int n)
@@ -59,9 +60,31 @@ int SmartMultiply(int num1,int num2) {
 	
 	
 }
+int FindPeak(vector<int> arr) {
+	int start = 0;
+	int end = arr.size()-1;
+	int mid = 0;
+		cout << "start " << start << endl;
+	cout << "end " << end << endl;
+	while (start < end) {
+		mid = start + (end -start) / 2;
+		if (arr[mid] >= arr[mid - 1] && arr[mid] >= arr[mid + 1])
+			return arr[mid];
+		else if (arr[mid] > arr[mid - 1] && arr[mid] < arr[mid + 1]) {
+			end = mid-1;
+
+		}
+		else {
+			start = mid+1;
+
+		}
+	}
+	return arr[end];
+	
+}
 int main()
 {
-	int num1, num2;
+	//int num1, num2;
 	/*cout << "Enter Number Of Elements" << endl;
 	int N;
 	cin >> N;
@@ -70,7 +93,7 @@ int main()
 	for (int i = 0; i < N; i++) {
 		cin >> ptr[i];
 	}
-	averageprefix(ptr, N);*/
+	averageprefix(ptr, N);
 	//insertionSort(ptr, N);
 	//bubbleSort(ptr, N);
 	//printArray(ptr, N);
@@ -78,7 +101,13 @@ int main()
 	cin >> num1;
 	cout << "Enter Num 2" << endl;
 	cin >> num2;
-	cout<<SmartMultiply(num1, num2);
+	cout<<SmartMultiply(num1, num2);*/
+	vector<int> arr = { 1, 3, 5, 7, 9, 11, 10, 8, 6, 4, 2 };
+	cout <<"Peak "<< FindPeak(arr) << endl;
+	vector<int> arr2 = { 1, 7, 6, 2 };
+	cout << "Peak " << FindPeak(arr2) << endl;
+	vector<int> arr3 = { 1, 2,3,4,5,6,7,11,10,9,8,7,5,6,4,3 };
+	cout << "Peak " << FindPeak(arr3) << endl;
 	return 0;
 }
 
